d14b7ce91c0c34cc3c81439eb2466f22c0fe4ee2
WilmerC27 <l18530441@cancun.tecnm.mx>
2023-04-13 19:59:46 -0500

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Pressable, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App=()=> {
+const nuevaCitaHandler=()=>{
+console.log('Presionaste el boton...')
 }
 
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={styles.titulo}>Administrador de citas {''}
+        <Text style={styles.tituloBold}>Veterinaria</Text>
+        </Text>
+        <Pressable style={styles.btnNuevaCita}
+        onPress={nuevaCitaHandler}>
+          <Text style={styles.btnTextoNuevaCita}>Nueva Cita</Text>
+        </Pressable>
+      </View>
+    </SafeAreaView>
+  );
+};
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    backgroundColor:'#F3F4F6',
+    flex:1
   },
+  titulo: {
+    textAlign:'center',
+    fontSize:30,
+    color:'#374151',
+    fontWeight:'bold'
+  },
+  tituloBold:{
+    fontWeight:900,
+    color:'#6D28D9',
+  },
+  btnNuevaCita:{
+    backgroundColor:'#6D28D9',
+    padding:15,
+    marginTop:20,
+    marginHorizontal:20,
+    borderRadius:10
+  },
+  btnTextoNuevaCita:{
+    textAlign:'center',
+    color:'#FFF',
+    fontSize:20,
+    fontWeight:'900',
+    textTransform:'uppercase'
+  }
 });
+export default App;
